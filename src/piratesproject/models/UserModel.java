5208a9f9110e40ffec0de9c6b9c0bd52ad12ce90
Mohamed taj Eldin Ahmed <mohamedtajeldinai@gmail.com>
2025-01-01 16:12:56 +0200

    adding usermodel class in developer

---
+++ src/piratesproject/models/UserModel.java
@@ -0,0 +1,104 @@
+package user_model;
+
+public class UserModel {
+    
+    String userFirstName; 
+    String userLastName;
+    String userName; 
+    String userPassword; 
+    String userIsOnline; 
+    String userIsplayingnow; 
+    String userTotalGamePlayed; 
+    int userTotalScore;
+    
+    
+    public String getUserFirstName() {
+        return userFirstName;
+    }
+
+    public void setUserFirstName(String userFirstName) {
+        this.userFirstName = userFirstName;
+    }
+
+    
+    public String getUserLastName() {
+        return userLastName;
+    }
+
+    public void setUserLastName(String userLastName) {
+        this.userLastName = userLastName;
+    }
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserIsOnline() {
+        return userIsOnline;
+    }
+
+    public void setUserIsOnline(String userIsOnline) {
+        this.userIsOnline = userIsOnline;
+    }
+    public int getUserTotalScore() {
+        return userTotalScore;
+    }
+
+    public void setUserTotalScore(int userTotalScore) {
+        this.userTotalScore = userTotalScore;
+    }
+
+    public String getUserIsplayingnow() {
+        return userIsplayingnow;
+    }
+
+    public void setUserIsplayingnow(String userIsplayingnow) {
+        this.userIsplayingnow = userIsplayingnow;
+    }
+
+    public String getUserTotalGamePlayed() {
+        return userTotalGamePlayed;
+    }
+
+    public void setUserTotalGamePlayed(String userTotalGamePlayed) {
+        this.userTotalGamePlayed = userTotalGamePlayed;
+    }
+    
+    // can add vector of history score of each users 
+     
+  
+    
+    public UserModel(String Fname , String username , String password ){
+        this.userFirstName = Fname ; 
+        this.userName = username ; 
+        this.userPassword = password; 
+    }
+    
+    public UserModel(String userFirstName  , String userLastName  , String userName,String userPasswor ){
+     
+        this.userFirstName = userFirstName; 
+        this.userLastName = userLastName;
+        this.userName = userName; 
+        this.userPassword = userPassword; 
+    }    
+    
+    public static void main(String[] args) {
+        UserModel myUser = new UserModel("Mohamed" , "Ahmed" , "username123" , "password"); 
+ 
+        
+    }
+
+}
