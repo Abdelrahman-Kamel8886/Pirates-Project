ae865b9fff8444999f4e08f796a9eb036856df35
Abdelrahman Kamel <abdelrahmankamel8886@gmail.com>
2025-01-02 08:13:16 +0200

    add logo , change the name - add SQLite file and Local DataAccessLayer

--- src/piratesproject/Main.java
+++ src/piratesproject/Main.java
@@ -1,18 +1,24 @@
 package piratesproject;
+
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import piratesproject.utils.Consts;
 
 public class Main extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = new BorderPane();
         Scene scene = new Scene(root);
+
         stage.setTitle(Consts.APP_NAME);
+        
+        stage.getIcons().add(new Image(getClass().getResource(Consts.APP_LOGO_PATH).toString()));
+        
         stage.setScene(scene);
         stage.show();
     }
@@ -20,5 +26,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
--- src/piratesproject/models/ResponseModel.java
+++ src/piratesproject/models/ResponseModel.java
@@ -5,6 +5,14 @@ public class ResponseModel {
     private String status;
     private String message;
 
+    public ResponseModel() {
+    }
+
+    public ResponseModel(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
     public String getStatus() {
         return status;
     }
