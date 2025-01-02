a3fdb665fc35cf5d6adbe7ff150ca252043b6044
Abdelrahman Kamel <abdelrahmankamel8886@gmail.com>
2025-01-02 10:12:01 +0200

    modify LoginResponseModel - ignore build and dist directories

--- src/piratesproject/models/LoginResponseModel.java
+++ src/piratesproject/models/LoginResponseModel.java
@@ -1,11 +1,22 @@
 package piratesproject.models;
 
 public class LoginResponseModel extends ResponseModel{
-
+    
+    private UserModel userModel ;
+    
     public LoginResponseModel() {
     }
-    public LoginResponseModel(String status, String message) {
+    public LoginResponseModel(String status, String message , UserModel user) {
         super(status, message);
+        this.userModel = user;
+    }
+
+    public UserModel getUserModel() {
+        return userModel;
+    }
+
+    public void setUserModel(UserModel userModel) {
+        this.userModel = userModel;
     }
     
 }
