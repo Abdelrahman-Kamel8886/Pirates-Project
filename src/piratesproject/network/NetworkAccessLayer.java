c4b0b4e8c3bbf0dd144eedb4e725e9d82cbed1eb
jailan <jailanmedhatt2@hmail.com>
2025-01-07 21:25:43 +0200

    private.properties

--- src/piratesproject/network/NetworkAccessLayer.java
+++ src/piratesproject/network/NetworkAccessLayer.java
@@ -24,7 +24,7 @@ import piratesproject.utils.JsonUtils;
 
 public class NetworkAccessLayer {
 
-    private static final String SERVER_HOST = "127.0.0.1"; // Change to your server's address
+    private static final String SERVER_HOST = "172.16.222.147"; // Change to your server's address
     private static final int SERVER_PORT = 1422; // Change to your server's port
 
     public static ResponseModel registerToServer(UserModel u) {
