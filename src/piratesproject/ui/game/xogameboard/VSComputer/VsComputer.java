97261754c7207060a5023156256df3d43003e6c9
Nourhan <nouressam5833@gmail.com>
2025-01-13 10:15:52 +0200

    fix conflict in levelcontroller class

--- src/piratesproject/ui/game/xogameboard/VSComputer/VsComputer.java
+++ src/piratesproject/ui/game/xogameboard/VSComputer/VsComputer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package piratesproject.ui.game.xogameboard.VSComp;
+package piratesproject.ui.game.xogameboard.VSComputer;
 
 import java.util.ArrayList;
 import java.util.List;
